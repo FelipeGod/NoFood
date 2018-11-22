1630c21c647702b7b827bacfcfffdab12e33bd31
Felipe <felipe.mschultz@hotmail.com>
2018-11-22 16:35:18 -0200

    Att

--- NoFood.Ionic/platforms/android/assets/www/build/main.js
+++ NoFood.Ionic/platforms/android/assets/www/build/main.js
@@ -181,8 +181,8 @@ var ProviderBase = /** @class */ (function () {
 var ConfigHelper = /** @class */ (function () {
     function ConfigHelper() {
     }
-    ConfigHelper.Url = 'http://felipemachadodev.com.br/api/';
-    // public static Url: string = '/v1/'
+    // public static Url: string = 'http://felipemachadodev.com.br/api/'
+    ConfigHelper.Url = '/v1/';
     //public static Url: string = 'http://localhost:3000/api/'
     ConfigHelper.storageKeys = {
         token: 'nofood.token',
@@ -564,7 +564,7 @@ var MyApp = /** @class */ (function () {
         });
     }
     MyApp = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["m" /* Component */])({template:/*ion-inline-start:"C:\Users\felipe.schultz\Desktop\NoFood.Ionic\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"C:\Users\felipe.schultz\Desktop\NoFood.Ionic\src\app\app.html"*/
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["m" /* Component */])({template:/*ion-inline-start:"E:\NoFood\NoFood.Ionic\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"E:\NoFood\NoFood.Ionic\src\app\app.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2_ionic_angular__["k" /* Platform */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_4__ionic_native_splash_screen__["a" /* SplashScreen */]])
     ], MyApp);
@@ -667,6 +667,10 @@ var UsuarioProvider = /** @class */ (function (_super) {
     UsuarioProvider.RegisterLogin = function (result) {
         localStorage.setItem(__WEBPACK_IMPORTED_MODULE_3__app_helpers_configHelper__["a" /* ConfigHelper */].storageKeys.token, result.token);
         localStorage.setItem(__WEBPACK_IMPORTED_MODULE_3__app_helpers_configHelper__["a" /* ConfigHelper */].storageKeys.user, JSON.stringify(result.usuario));
+    };
+    UsuarioProvider.RemoveLogin = function () {
+        localStorage.removeItem(__WEBPACK_IMPORTED_MODULE_3__app_helpers_configHelper__["a" /* ConfigHelper */].storageKeys.token);
+        localStorage.removeItem(__WEBPACK_IMPORTED_MODULE_3__app_helpers_configHelper__["a" /* ConfigHelper */].storageKeys.user);
     };
     Object.defineProperty(UsuarioProvider, "GetTokenAccess", {
         get: function () {
--- NoFood.Ionic/www/build/6.js
+++ NoFood.Ionic/www/build/6.js
@@ -72,7 +72,7 @@ var MeusPedidosPage = /** @class */ (function () {
     };
     MeusPedidosPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-meus-pedidos',template:/*ion-inline-start:"C:\Users\felipe.schultz\Desktop\NoFood.Ionic\src\pages\meus-pedidos\meus-pedidos.html"*/'<!--\n  Generated template for the MeusPedidosPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>meusPedidos</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"C:\Users\felipe.schultz\Desktop\NoFood.Ionic\src\pages\meus-pedidos\meus-pedidos.html"*/,
+            selector: 'page-meus-pedidos',template:/*ion-inline-start:"E:\NoFood\NoFood.Ionic\src\pages\meus-pedidos\meus-pedidos.html"*/'<!--\n  Generated template for the MeusPedidosPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>meusPedidos</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"E:\NoFood\NoFood.Ionic\src\pages\meus-pedidos\meus-pedidos.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* NavParams */]])
     ], MeusPedidosPage);
